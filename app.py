d9c11b6b647be4747896608d7110422482d692e9
Tiru21 <fedorarhipenkov@yandex.ru>
2021-05-04 14:56:53 +0300

    v1

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import sqlite3
 import spatialite
-#conn = sqlite3.connect(":memory:")
-#conn.enable_load_extension(True)
+import json
 db = sqlite3.connect("db1.sqlite")
 db.enable_load_extension(True)
 db.load_extension('mod_spatialite')
-print(db.execute('SELECT spatialite_version()').fetchone()[0])
+db.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
 strsql = """
 select Distance(GeomFromText('POINT (50.97598 60.43534)', 4326),
             GEOMETRYPLACE, 1) AS DIST, TYPETM, NAMETM, KLASSVOLTAGE, PARTVL, NAMEVL, MANAGE,
@@ -16,5 +15,8 @@ select Distance(GeomFromText('POINT (50.97598 60.43534)', 4326),
             GEOMETRYPLACE, 100,1)
 """
 curr = db.cursor()
-for row in curr.execute(strsql):
-    print(row)
+curr.execute(strsql)
+data = curr.fetchall()
+print(json.dumps([dict(el) for el in data]))
+db.close()
+
